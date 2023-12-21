78fe271104e6760888bc22b21a2ba8974fa44672
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-21 22:08:51 +0900

    function 제거

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-
-exports.scheduledFunction = functions.pubsub
-  .schedule("every 1 minutes")
-  .onRun((context) => {
-    console.log("This function is running every minute!");
-    logger.info("Hello logs!");
-    // 여기에 원하는 로직을 추가합니다.
-  });
