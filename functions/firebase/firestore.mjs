857b31877e4eca0fa53d1b6d17163b8b856e7bfc
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-22 15:15:40 +0900

    background로 변경

--- functions/firebase/firestore.mjs
+++ functions/firebase/firestore.mjs
@@ -2,7 +2,7 @@ import { db } from "./base.mjs";
 
 export const saveFirestore = async (data) => {
   for (const d of data) {
-    const docRef = db.collection("posts").doc(d.id);
+    const docRef = db.collection("table").doc(d.id);
     console.log(d.id);
     await docRef.set({
       title: d.title,
