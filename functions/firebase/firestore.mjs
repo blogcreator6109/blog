3f790efc25424a5e69b3eef9ce37e5530b79e0c2
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-22 15:08:10 +0900

    update

--- functions/firebase/firestore.mjs
+++ functions/firebase/firestore.mjs
@@ -2,9 +2,10 @@ import { db } from "./base.mjs";
 
 export const saveFirestore = async (data) => {
   for (const d of data) {
-    const docRef = db.collection("posts").doc(r.id);
+    const docRef = db.collection("posts").doc(d.id);
+    console.log(d.id);
     await docRef.set({
-      title: r.title,
+      title: d.title,
     });
   }
 };
