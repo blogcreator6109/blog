784d88b6b4159f6eeba776235b680127f946e00e
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-27 14:49:52 +0900

    useSEOMeta에 onMounted 추가

--- pages/post/[postId].vue
+++ pages/post/[postId].vue
@@ -13,22 +13,24 @@ const { post } = storeToRefs(postStore);
 
 postStore.setView("content");
 
-useSeoMeta({
-  title: post.value?.title,
-  ogTitle: post.value?.title,
-  ogType: "article",
-  description: post.value?.description,
-  ogDescription: post.value?.description,
-  ogImage: post.value?.cover,
-  ogSiteName: "Blog Creator's blog",
-  ogUrl: window.location.href,
-  ogLocale: "ko_KR",
-  articleAuthor: "Blog Creator",
-  twitterCard: "summary_large_image",
-  twitterTitle: post.value?.title,
-  twitterDescription: post.value?.description,
-  twitterImage: post.value?.cover,
-  twitterCreator: "@BlogCreator6109",
+onMounted(() => {
+  useSeoMeta({
+    title: post.value?.title,
+    ogTitle: post.value?.title,
+    ogType: "article",
+    description: post.value?.description,
+    ogDescription: post.value?.description,
+    ogImage: post.value?.cover,
+    ogSiteName: "Blog Creator's blog",
+    ogUrl: window.location.href,
+    ogLocale: "ko_KR",
+    articleAuthor: "Blog Creator",
+    twitterCard: "summary_large_image",
+    twitterTitle: post.value?.title,
+    twitterDescription: post.value?.description,
+    twitterImage: post.value?.cover,
+    twitterCreator: "@BlogCreator6109",
+  });
 });
 
 openWindow("Post");
