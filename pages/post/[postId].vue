bc952ba89d41cba20c25c64eb6fe87979790b95f
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-27 14:31:06 +0900

    useSEOMeta

--- pages/post/[postId].vue
+++ pages/post/[postId].vue
@@ -1,19 +1,35 @@
 <template>
-  <Meta
-    v-if="postStore.post"
-    :post="postStore.post"
-    :url="'/post/' + postStore.post.number"
-  />
+  <noscript></noscript>
 </template>
 
 <script setup>
-import Meta from "~/components/Meta.vue";
 import { useWindowStore } from "@/stores/window";
 import { usePostStore } from "@/stores/post";
+import { storeToRefs } from "pinia";
 
 const { openWindow } = useWindowStore();
 const postStore = usePostStore();
+const { post } = storeToRefs(postStore);
+
 postStore.setView("content");
+
+useSeoMeta({
+  title: post.value?.title,
+  ogTitle: post.value?.title,
+  ogType: "article",
+  description: post.value?.description,
+  ogDescription: post.value?.description,
+  ogImage: post.value?.cover,
+  ogSiteName: "Blog Creator's blog",
+  ogUrl: window.location.href,
+  ogLocale: "ko_KR",
+  articleAuthor: "Blog Creator",
+  twitterCard: "summary_large_image",
+  twitterTitle: post.value?.title,
+  twitterDescription: post.value?.description,
+  twitterImage: post.value?.cover,
+  twitterCreator: "@BlogCreator6109",
+});
 
 openWindow("Post");
 </script>
