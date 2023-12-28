79f1d36b4ccbfed2650709854a558d43f5199103
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-28 17:47:07 +0900

    post content list 바뀔 때 scroll 위로 옮기기

--- components/windows/Post.vue
+++ components/windows/Post.vue
@@ -6,6 +6,15 @@ import { usePostStore } from "~/stores/post";
 
 const postStore = usePostStore();
 
+watch(
+  () => postStore.view,
+  () => {
+    if (document) {
+      document.querySelector(".window .body").scrollTop = 0;
+    }
+  }
+);
+
 onBeforeMount(() => {
   // 닫히기전에 사이드바가 열려있었다면 닫아준다
   postStore.closeSidebar();
@@ -13,7 +22,7 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="post" ref="post">
+  <div class="post">
     <SideBar />
     <Content v-if="postStore.view == 'content'" />
     <List v-else-if="postStore.view == 'list'" />
