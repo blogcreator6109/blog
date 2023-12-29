11ab1133d9c15f3f8ad3fec8cd21f7c2803e36f0
blogcreator6109 <blogcreator6109@gmail.com>
2023-12-29 19:19:09 +0900

    visitors 호출을 쿠키에 따라 하지 않도록 설정

--- server/api/firebase/visitors.js
+++ server/api/firebase/visitors.js
@@ -1,23 +1,14 @@
 // server/middleware/session.js
 import admin from "firebase-admin";
-import { v4 as uuidv4 } from "uuid";
 
-export default defineEventHandler(async (e) => {
-  let sessionId = getCookie(e, "sessionId");
-  // let sessionId = null;
-
-  if (!sessionId) {
-    // 1시간 동안 유효한 쿠키를 생성합니다
-    // 그 뒤에 또 들어온다면 새로운 방문자로 취급할거에요!
-    sessionId = uuidv4(); // 새로운 UUID 생성
-    setCookie(e, "sessionId", sessionId, {
-      httpOnly: true,
-      path: "/",
-      maxAge: 60 * 60,
-    });
-
+export default defineEventHandler(async () => {
+  try {
+    console.log("Visitors 기록");
     const db = admin.firestore();
     await db.collection("visitors").doc(sessionId).set({});
+
+    return true;
+  } catch (e) {
+    return false;
   }
-  return e;
 });
